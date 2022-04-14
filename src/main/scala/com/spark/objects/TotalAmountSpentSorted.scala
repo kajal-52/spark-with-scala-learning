0b4fee7577e9c1074a0ff79d20b84c62892787db
Kajal Tiwari <kajal.tiwari348@gmail.com>
2022-04-14 23:41:28 +0530

    Exercise for RDD functions

---
+++ src/main/scala/com/spark/objects/TotalAmountSpentSorted.scala
@@ -0,0 +1,33 @@
+package com.spark.objects
+
+import org.apache.log4j.{Level, Logger}
+import org.apache.spark.SparkContext
+
+object TotalAmountSpentSorted {
+  def extractCustomerPricePairs(line: String): (Int, Float)={
+    val customerFields=line.split(",")
+    (customerFields(0).toInt,customerFields(2).toFloat)
+  }
+  def main(args: Array[String]): Unit = {
+
+    Logger.getLogger("org").setLevel(Level.INFO)
+
+    val sc = new SparkContext("local[*]", "TotalAmountSpentByCustomer")
+
+    val customerOrders = sc.textFile("src/main/resources/customer-orders.csv")
+    val elementOfOrders = customerOrders.map(extractCustomerPricePairs)
+    val amountSpentByCustomer = elementOfOrders.reduceByKey(( x, y ) => ( x + y ))
+
+    //Sort results by amount spend by customer
+    val amountSpentByCustomerPair = amountSpentByCustomer.map( x => (x._2, x._1))
+    val amountSpentByCustomerSorted = amountSpentByCustomerPair.sortByKey()
+
+    val totalAmountSpentByCustomer = amountSpentByCustomerSorted.collect()
+    totalAmountSpentByCustomer.foreach(println)
+
+
+
+
+  }
+
+}
